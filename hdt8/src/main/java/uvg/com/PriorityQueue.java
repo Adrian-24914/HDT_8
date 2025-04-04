154be83d0430dced4bb073d29f160a9de8a6c468
Andres Ismalej <andesteisma@gmail.com>
2025-04-03 21:56:59 -0600

    Cambios finales

--- hdt8/src/main/java/uvg/com/PriorityQueue.java
+++ hdt8/src/main/java/uvg/com/PriorityQueue.java
@@ -6,7 +6,6 @@
 */
 package uvg.com;
 
-<<<<<<< HEAD
 /**
  * Interfaz para una cola con prioridad genérica.
  * @param <E> Tipo de elementos que se almacenarán en la cola
@@ -48,31 +47,3 @@ public interface PriorityQueue<E extends Comparable<E>> {
      */
     void clear();
 }
-=======
-package hdt8.src.main.java.uvg.com;
-import java.io.*;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-
-public class PriorityQueue {
-    public static void main(String[] args) {
-        PriorityQueue<Paciente> cola = new PriorityQueue<>();
-        
-        try (Scanner scanner = new Scanner(new File("pacientes.txt"))) {
-            while (scanner.hasNextLine()) {
-                String[] datos = scanner.nextLine().split(", ");
-                cola.add(new Paciente(datos[0], datos[1], datos[2].charAt(0)));
-            }
-        } catch (FileNotFoundException e) {
-            System.err.println("Archivo pacientes.txt no encontrado.");
-            return;
-        }
-
-        System.out.println("Atendiendo pacientes en orden de prioridad:");
-        while (!cola.isEmpty()) {
-            System.out.println(cola.poll());
-        }
-    }
-}
-
->>>>>>> 5cf561d48c861c7a83697018814a2cbf3b1d9011
